c7b08c8e99051229abd9cb4b89823ec0d2c71d3a
Enzo García <enzo@digitalhouse.com>
2021-04-29 13:22:40 -0300

    implement pg-swagger-kit and configure static ui deployment

--- docker/local/init.sh
+++ docker/local/init.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-cd swagger
-npm install
-pm2 start server.js --watch
-
-cd ..
+pm2 start node_modules/@dhteam/pg-swagger-kit/src/server.js --watch
 npm install
 npm run migrate
 sls offline start
-
--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,4 +1,7 @@
 module.exports = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT',
+    'Access-Control-Allow-Headers':
+        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,TenantId',
 }
