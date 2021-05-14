284293e3585ff2381ae060b3fb0f047d290ce77d
Alexis Bianchi <abianchi@digitalhouse.com>
2021-05-14 17:19:44 -0300

    PGD-1600 unify tenant-id header

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -3,5 +3,5 @@ module.exports = {
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT',
     'Access-Control-Allow-Headers':
-        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,TenantId',
+        'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,tenant-id',
 }
